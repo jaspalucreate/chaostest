dc9c3e243bc31526b484fae730df8e6122008cc1
jaspal singh <jaspal@ucreate.co.in>
2019-01-11 11:11:59 +0530

    added new relic package

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+require('newrelic');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const rollbar = require('rollbar');
-require('newrelic');
+const dotenv = require('dotenv');
+dotenv.load();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
