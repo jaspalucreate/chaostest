b95f62da474f2ec8e919b215dfe1e75e6ac8c736
jaspal singh <jaspal@ucreate.co.in>
2019-01-11 12:00:19 +0530

    Fixed the issue to load the env variables

--- newrelic.js
+++ newrelic.js
@@ -1,28 +1,36 @@
 'use strict'
+const dotenv = require('dotenv');
+dotenv.load();
 /**
- * New Relic agent configuration.
- *
- * See lib/config.defaults.js in the agent distribution for a more complete
- * description of configuration variables and their potential values.
+ * This file includes all of the configuration variables used by the Node.js
+ * module. If there's a configurable element of the module and it's not
+ * described in here, there's been a terrible mistake.
  */
-if (process.env.ENVIRONMENT != environmentServer.Development) {
-  exports.config = {
-    /**
-     * Array of application names.
-     */
-    app_name: [process.env.APP_NAME],
+exports.config = {
+  /**
+   * Array of application names.
+   *
+   * @env NEW_RELIC_APP_NAME
+   */
+  app_name: [process.env.APP_NAME],
+  /**
+   * The user's license key. Must be set by per-app configuration file.
+   *
+   * @env NEW_RELIC_LICENSE_KEY
+   */
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
 
+  logging: {
     /**
-     * Your New Relic license key.
+     * Verbosity of the module's logging. This module uses bunyan
+     * (https://github.com/trentm/node-bunyan) for its logging, and as such the
+     * valid logging levels are 'fatal', 'error', 'warn', 'info', 'debug' and
+     * 'trace'. Logging at levels 'info' and higher is very terse. For support
+     * requests, attaching logs captured at 'trace' level are extremely helpful
+     * in chasing down bugs.
+     *
+     * @env NEW_RELIC_LOG_LEVEL
      */
-    license_key: process.env.NEW_RELIC_LICENSE_KEY,
-    logging: {
-      /**
-       * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-       * issues with the agent, 'info' and higher will impose the least overhead on
-       * production applications.
-       */
-      level: 'info'
-    }
+    level: 'info'
   }
 }
